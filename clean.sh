a3dc85d34b877f16b692daf532b0045753d0bf9a
ampholyte <schmitt.matthieu.89@gmail.com>
2013-09-20 10:38:00 +0200

    Refonte arborescence

--- clean.sh
+++ clean.sh
@@ -1,1 +1,23 @@
+#!/bin/sh
 find . -name "*~" | xargs rm -r
+
+out=$(find . -name "*.ly")
+
+for i in $out
+do
+    if test "$i" != "./template.ly"
+    then lilypond $i
+    fi
+done
+
+out=$(find . -name "*.pdf")
+
+for i in $out
+do mv $i pdf/.
+done
+
+out=$(find . -name "*.ps")
+
+for i in $out
+do mv $i ps/.
+done
